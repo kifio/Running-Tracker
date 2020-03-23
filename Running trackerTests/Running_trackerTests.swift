a5ebb106a36c7226a2cec7e364300a0a796320dc
imurashov <ivan.murashov@omio.com>
2020-03-23 12:13:05 +0100

    Initial Commit

---
+++ Running trackerTests/Running_trackerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Running_trackerTests.swift
+//  Running trackerTests
+//
+//  Created by Ivan Murashov on 23.03.20.
+//  Copyright © 2020 Ivan Murashov. All rights reserved.
+//
+
+import XCTest
+@testable import Running_tracker
+
+class Running_trackerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
